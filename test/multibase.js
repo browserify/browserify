59e8cc8b84a22ceccd4cea4da32c0f0748b207d6
James Halliday <substack@gmail.com>
2011-03-28 07:13:40 -0700

    testing multiple bases for arrays too, everything passes

--- test/multibase.js
+++ test/multibase.js
@@ -23,3 +23,26 @@ exports.multibaseObject = function () {
         333
     );
 };
+
+exports.multibaseArray = function () {
+    var src = browserify.bundle({
+        base : [
+            __dirname + '/pkg/a',
+            __dirname + '/pkg/c',
+        ],
+    });
+    
+    var c = {};
+    Script.runInNewContext(src, c);
+    
+    assert.eql(
+        Script.runInNewContext('require("./moo").zzz(3)', c),
+        333
+    );
+    
+    assert.eql(
+        Script.runInNewContext('require("doom").fn(3)', c),
+        300
+    );
+
+};
