bf5c3c4b48f4cce1ae5f34862e8cf855df0e8208
James Halliday <mail@substack.net>
2011-05-09 23:15:03 -0700

    failing node_modules test

---
+++ test/modules.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var browserify = require('browserify');
+var vm = require('vm');
+var Hash = require('hashish');
+
+exports.modules = function () {
+    var src = browserify.bundle({
+        base : {
+            'foomoduletest' : __dirname + '/modules/foomoduletest'
+        }
+    });
+    
+    assert.ok(typeof src === 'string');
+    assert.ok(src.length > 0);
+    
+    var c = {};
+    vm.runInNewContext(src, c);
+    assert.eql(c.require('foomoduletest').bar, 55)
+};
