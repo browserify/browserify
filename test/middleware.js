e3c937c0964bbf9890e4ca09a527306eefad8b03
James Halliday <mail@substack.net>
2011-05-27 17:23:00 -0700

    failing middleware test

---
+++ test/middleware.js
@@ -0,0 +1,27 @@
+var assert = require('assert');
+var browserify = require('browserify');
+var vm = require('vm');
+
+exports.middleware = function () {
+    var bundle = browserify(__dirname + '/middleware');
+    bundle.use(function (src, next) {
+        setTimeout(function () {
+            next(src + '\nvar yyy = 444;');
+        }, 50);
+    });
+    bundle.use(function (src, next) {
+        setTimeout(function () {
+            next(src + '\nvar zzz = yyy + 111;');
+        }, 50);
+    });
+    
+    var to = setTimeout(function () {
+        assert.fail('middleware never ready');
+    }, 5000);
+    
+    bundle.on('ready', function (src) {
+        var c = {};
+        vm.runInNewContext(src, c);
+        assert.equal(c.require('./'), 555);
+    });
+};
