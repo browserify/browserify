3f4cc3498d2d9b85d40af810bfbb069c795d92f5
James Halliday <mail@substack.net>
2011-05-28 12:15:18 -0700

    failing test for node_modules precedence order

--- test/modules.js
+++ test/modules.js
@@ -17,3 +17,23 @@ exports.modules = function () {
     vm.runInNewContext(src, c);
     assert.eql(c.require('foomoduletest').bar, 55)
 };
+
+exports.precedence = function () {
+    var src = browserify.bundle({
+        base : __dirname + '/modules/precedence',
+        require : [ 'hashish' ],
+    });
+    
+    var c = {};
+    vm.runInNewContext(src, c);
+    assert.ok(c.require.modules.hashish);
+    assert.deepEqual(
+        c.require('hashish').map(
+            { a : 1, b :2 }, function (x) { return x * 100 }
+        ),
+        { a : 100, b : 200 }
+    );
+    assert.ok(c.require.modules['./node_modules/hashish/index']);
+    assert.notEqual(c.require('hashish'), c.require('./node_modules/hashish'));
+    assert.equal(c.require('./x'), 'meow');
+};
