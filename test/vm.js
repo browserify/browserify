3be2d8ce6652c18b344bee7ba781a5711a76c3f2
James Halliday <mail@substack.net>
2011-06-04 20:07:09 -0700

    failing test for vm.runInNewContext: context not modified afterwards

--- test/vm.js
+++ test/vm.js
@@ -5,9 +5,14 @@ var browserify = require('browserify');
 exports.vm = function () {
     var src = browserify.bundle();
     
-    var c = {};
-    vm.runInNewContext(src, c);
+    var c0 = {};
+    vm.runInNewContext(src, c0);
     
-    assert.ok(c.require.modules.vm);
-    var vm_ = c.require('vm');
+    assert.ok(c0.require.modules.vm);
+    var vm0 = c0.require('vm');
+    
+    // so meta...
+    var c1 = {};
+    var vm1 = vm0.runInNewContext(src, c1);
+    assert.ok(c1.require.modules.vm);
 };
