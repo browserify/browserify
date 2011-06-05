699a028671dcaff3ef6f5c89fe465d487887e83e
James Halliday <mail@substack.net>
2011-06-05 04:11:04 -0700

    dependencies seem to be computed properly

--- wrappers/body.js
+++ wrappers/body.js
@@ -1,23 +1,22 @@
-_browserifyRequire.modules[$filename] = function () {
+_browserifyRequire.modules[$__filename] = function () {
     var module = { exports : {} };
     var exports = module.exports;
     var __dirname = $__dirname;
     var __filename = $__filename;
     
     var require = function (path) {
-        return _browserifyRequire.fromFile($filename, path);
+        return _browserifyRequire.fromFile($__filename, path);
     };
     require.modules = _browserifyRequire.modules;
-    _browserifyRequire.modules[$filename]._cached = module.exports;
+    require.resolve = function (name) {
+        return _browserifyRequire.resolve(name, $__dirname);
+    };
+    _browserifyRequire.modules[$__filename]._cached = module.exports;
     
     (function () {
         $body;
     }).call(module.exports);
     
-    _browserifyRequire.modules[$filename]._cached = module.exports;
+    _browserifyRequire.modules[$__filename]._cached = module.exports;
     return module.exports;
 };
-
-$aliases.forEach(function (a) {
-    _browserifyRequire.modules[a] = _browserifyRequire.modules[$filename];
-});
