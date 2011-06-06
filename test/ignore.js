49a0f6491e7ff69131253f7ab5baf3e1597133d1
James Halliday <mail@substack.net>
2011-06-05 19:01:38 -0700

    failing test for explicit bin dir

---
+++ test/ignore.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var vm = require('vm');
+var fs = require('fs');
+var browserify = require('browserify');
+
+exports.explicitIgnore = function () {
+    var src = browserify.bundle(__dirname + '/ignore/explicit');
+    var c = {};
+    vm.runInNewContext(src, c);
+    
+    var files = Object.keys(c.require.modules)
+        .filter(function (file) { return file.match(/^\./) })
+    ;
+    assert.deepEqual(files.sort(), [
+        './package.json',
+        './x/x.js',
+        './y.js',
+    ]);
+    console.dir(files);
+};
