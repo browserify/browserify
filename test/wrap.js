bc2d95001edead353f0df92745a0330682f49b35
James Halliday <mail@substack.net>
2011-06-19 02:11:53 -0700

    synchornous interface for walk()

--- test/wrap.js
+++ test/wrap.js
@@ -3,42 +3,30 @@ var path = require('path');
 var wrapper = require('../lib/wrap');
 
 exports.wrap = function () {
-    var to = setTimeout(function () {
-        assert.fail('never walked');
-    }, 10000);
+    var files = wrapper.walk(__dirname + '/wrap/a.js');
     
-    wrapper.walk(__dirname + '/wrap/a.js', function (err, files) {
-        clearTimeout(to);
-        if (err) assert.fail(err);
-        
-        assert.deepEqual(Object.keys(files).sort(), [
-            __dirname + '/wrap/a.js',
-            __dirname + '/wrap/node_modules/b/main.js',
-            __dirname + '/wrap/c.js',
-            __dirname + '/wrap/x.js',
-            path.resolve(__dirname, '../builtins/vm.js'),
-        ].sort());
-    });
+    assert.deepEqual(Object.keys(files).sort(), [
+        __dirname + '/wrap/a.js',
+        __dirname + '/wrap/node_modules/b/main.js',
+        __dirname + '/wrap/c.js',
+        __dirname + '/wrap/x.js',
+        path.resolve(__dirname, '../builtins/vm.js'),
+    ].sort());
 };
 
 exports.wrapArray = function () {
-    var to = setTimeout(function () {
-        assert.fail('never walked');
-    }, 10000);
+    var files = wrapper.walk([
+        __dirname + '/wrap/a.js',
+        __dirname + '/wrap/skipme.js',
+    ]);
     
-    wrapper.walk([ __dirname + '/wrap/a.js', __dirname + '/wrap/skipme.js' ],
-    function (err, files) {
-        clearTimeout(to);
-        if (err) assert.fail(err);
-        
-        assert.deepEqual(Object.keys(files).sort(), [
-            __dirname + '/wrap/a.js',
-            __dirname + '/wrap/node_modules/b/main.js',
-            __dirname + '/wrap/c.js',
-            __dirname + '/wrap/x.js',
-            __dirname + '/wrap/skipme.js',
-            __dirname + '/wrap/node_modules/skipmetoo/index.js',
-            path.resolve(__dirname, '../builtins/vm.js'),
-        ].sort());
-    });
+    assert.deepEqual(Object.keys(files).sort(), [
+        __dirname + '/wrap/a.js',
+        __dirname + '/wrap/node_modules/b/main.js',
+        __dirname + '/wrap/c.js',
+        __dirname + '/wrap/x.js',
+        __dirname + '/wrap/skipme.js',
+        __dirname + '/wrap/node_modules/skipmetoo/index.js',
+        path.resolve(__dirname, '../builtins/vm.js'),
+    ].sort());
 };
