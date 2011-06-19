cc91e980da6910733d831ad7599e053f6970be9d
James Halliday <mail@substack.net>
2011-06-18 22:48:41 -0700

    passing test for lib/wrap

--- test/wrap.js
+++ test/wrap.js
@@ -1,11 +1,18 @@
 var assert = require('assert');
+var path = require('path');
 var wrapper = require('../lib/wrap');
 
 exports.wrap = function () {
-    wrapper(__filename, function (err, files) {
+    wrapper(__dirname + '/wrap/a.js', function (err, files) {
         if (err) assert.fail(err);
         
-        console.log('doom');
-        console.dir(Object.keys(files));
+        assert.deepEqual(Object.keys(files).sort(), [
+            __dirname + '/wrap/a.js',
+            __dirname + '/wrap/node_modules/b/main.js',
+            __dirname + '/wrap/c.js',
+            __dirname + '/wrap/x.js',
+            path.resolve(__dirname, '../builtins/vm.js'),
+        ].sort());
+        
     });
 };
