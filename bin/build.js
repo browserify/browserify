31e185f77807ac9b3d86098a3d76b1bd52ae70b9
James Halliday <mail@substack.net>
2011-07-14 14:58:43 -0700

    stub option parsing

--- bin/build.js
+++ bin/build.js
@@ -3,21 +3,40 @@
 var browserify = require('../');
 
 var argv = require('optimist')
-    .default('b', __dirname)
-    .alias('b', 'basepath')
-    .describe('b', 'The base path of the app you want to Browserify.')
-    .default('outpath', '/../')
-    .describe('outpath', 'The relative path to save your Browserified code to.')
-    .default('o', 'browserified.js')
-    .alias('o', 'outfile')
-    .describe('o', 'The filename to save your Browserified code as.')
-    .demand(['s', 't'])
-    .alias('t', 'templates')
-    .alias('s', 'main')
-    .describe('t', 'The relative path where your HTML templates reside.')
-    .describe('s', 'The relative path of the main script / entry point of your app.')
+    .usage('Usage: $0 [entry files] {OPTIONS}')
+    .wrap(80)
+    .option('base', {
+        alias : 'b',
+        default : process.cwd(),
+        desc : 'Base path of the app you want to browserify',
+    })
+    .option('outfile', {
+        alias : 'o',
+        desc : 'Write the browserify bundle to this file',
+        default : 'bundle.js',
+    })
+    .option('require', {
+        alias : 'r',
+        desc : 'A module name or file to bundle.require()\n'
+            + 'Optionally use a colon separator to set the target.'
+        ,
+    })
+    .option('entry', {
+        alias : 'e',
+        desc : 'An entry point of your app'
+    })
+    .option('help', {
+        alias : 'h',
+        desc : 'Show this message'
+    })
+    .check(function (argv) {
+        if (argv.help) throw ''
+        if (process.argv.length <= 2) throw 'Specify a parameter.'
+    })
     .argv
 ;
+
+process.exit();
 
 var fs = require('fs');
 var path = require('path');
