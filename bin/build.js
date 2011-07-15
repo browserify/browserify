38d7433942ab233c9b628d0fc674ac26d0517a38
James Halliday <mail@substack.net>
2011-07-14 18:17:05 -0700

    command line tool: now with plugins

--- bin/build.js
+++ bin/build.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
 var browserify = require('../');
+var fs = require('fs');
+var path = require('path');
 
 var argv = require('optimist')
     .usage('Usage: $0 [entry files] {OPTIONS}')
     .wrap(80)
-    .option('base', {
-        alias : 'b',
-        default : process.cwd(),
-        desc : 'Base path of the app you want to browserify',
-    })
     .option('outfile', {
         alias : 'o',
         desc : 'Write the browserify bundle to this file',
@@ -25,6 +22,18 @@ var argv = require('optimist')
         alias : 'e',
         desc : 'An entry point of your app'
     })
+    .option('alias', {
+        alias : 'a',
+        desc : 'Register an alias with a colon separator: "to:from"\n'
+            + "Example: --alias 'jquery:jquery-browserify'"
+        ,
+    })
+    .option('plugin', {
+        alias : 'p',
+        desc : 'Use a plugin. Use a colon separator to specify additional '
+            + 'plugin arguments as a JSON string.\n'
+            + 'Example: --plugin \'fileify:["files","."]\''
+    })
     .option('help', {
         alias : 'h',
         desc : 'Show this message'
@@ -36,29 +45,51 @@ var argv = require('optimist')
     .argv
 ;
 
-process.exit();
-
-var fs = require('fs');
-var path = require('path');
-
-var bundle = browserify(),
-    basePath = argv.b,
-    templatePath = argv.t,
-    entryPoint = argv.s,
-    outPath = argv.outpath,
-    outFile = argv.o;
-
-console.log(basePath);
-console.log('Generating bundle... ', basePath, templatePath);
-
-bundle.use(formify(path.normalize(basePath + templatePath), { watch : false }));
-bundle.require(path.normalize(basePath + entryPoint), {target: outFile});
-
-fs.writeFileSync(path.normalize(basePath + outPath + '/' + outFile), bundle.bundle());
-
-function formify() {
-  var fileify = require('fileify');
-  return function (bundle) {
-    bundle.use(fileify(path.normalize(templatePath + 'index.js'), path.normalize(basePath + templatePath), '.html'));
-  };
+if (path.existsSync(argv.outfile)) {
+    console.error('outfile exists.');
+    process.exit();
 }
+
+var bundle = browserify();
+([].concat(argv.plugin || [])).forEach(function (plugin) {
+    if (plugin.match(/:/)) {
+        var ps = plugin.split(':');
+        var args = ps[1];
+        try {
+            args = JSON.parse(args);
+        }
+        catch (err) {}
+        
+        if (!Array.isArray(args)) args = [ args ];
+        
+        var fn = require(ps[0]);
+        bundle.use(fn.apply(null, args));
+    }
+    else {
+        bundle.use(require(plugin));
+    }
+});
+
+([].concat(argv.alias || [])).forEach(function (alias) {
+    if (!alias.match(/:/)) {
+        console.error('aliases require a colon separator');
+        process.exit();
+    }
+    bundle.alias.apply(bundle, alias.split(':'));
+});
+
+([].concat(argv.require || [])).forEach(function (req) {
+    if (req.match(/:/)) {
+        var s = req.split(':');
+        bundle.require(s[0], { target : s[1] });
+    }
+    else {
+        bundle.require(req);
+    }
+});
+
+(argv._.concat(argv.entry || [])).forEach(function (entry) {
+    bundle.addEntry(entry);
+});
+
+fs.writeFileSync(argv.outfile, bundle.bundle());
