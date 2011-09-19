cd551e675591da117281a8c9a20b95c000a02383
George Stagas <gstagas@gmail.com>
2011-09-20 01:16:40 +0300

    improved process.nextTick

--- wrappers/process.js
+++ wrappers/process.js
@@ -1,8 +1,51 @@
 if (typeof process === 'undefined') process = {};
 
-if (!process.nextTick) process.nextTick = function (fn) {
-    setTimeout(fn, 0);
-};
+if (!process.nextTick) process.nextTick = (function () {
+    var queue = [];
+    var dirty = false;
+    var fn;
+    var hasPostMessage = !!window.postMessage;
+    var messageName = 'nexttick';
+    var trigger = (function () {
+        return hasPostMessage
+            ? function trigger () {
+                window.postMessage(messageName, '*');
+              }
+            : function trigger () {
+                setTimeout(function () { processQueue() }, 0);
+              };
+    }());
+    var processQueue = (function () {
+        return hasPostMessage
+            ? function processQueue (event) {
+                  if (event.source === window && event.data === messageName) {
+                      event.stopPropagation();
+                      flushQueue();
+                  }
+              }
+            : flushQueue;
+    })();
+    function flushQueue () {
+        while (fn = queue.shift()) {
+            fn();
+        }
+        dirty = false;
+    }    
+    function nextTick (fn) {
+        queue.push(fn);
+        if (dirty) return;
+        dirty = true;
+        trigger();
+    }
+
+    if (hasPostMessage) window.addEventListener('message', processQueue, true);
+
+    nextTick.removeListener = function () {
+        window.removeEventListener('message', processQueue, true);
+    }
+
+    return nextTick;
+})();
 
 if (!process.title) process.title = 'browser';
 
