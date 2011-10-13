70458663e11d6bc918485c685b270875c697366f
James Halliday <mail@substack.net>
2011-10-12 23:39:52 -0700

    get rid of crufty examples

--- example/entry/js/entry.js
+++
@@ -1,9 +0,0 @@
-// entry point for browser code
-
-var foo = require('./foo');
-
-window.onload = function () {
-    document.getElementById('result').innerHTML = foo(100);
-};
-
-if (window.document.readyState === 'complete') window.onload();
