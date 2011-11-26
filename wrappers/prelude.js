94b01e292e3cffc6230d076b3c818ff18dbe8f86
Lorenz Schori <lo@znerol.ch>
2011-11-26 19:56:52 +0100

    Fix loading toplevel directory from subdir in prelude
    The loadAsDirectory function of the prelude script prepended to
    browserified modules strips off trailing slashes before concatenating
    the argument with '/package.js' and '/index'. If the argument is a
    single slash, i.e. a path pointing to the root directory, it should be
    preserved when passing to path.resolve subsequently.
    
    Fix this problem by stripping off trailing slashes only for
    package/index lookup.

--- wrappers/prelude.js
+++ wrappers/prelude.js
@@ -51,8 +51,8 @@ require.resolve = (function () {
         }
         
         function loadAsDirectorySync (x) {
-            x = x.replace(/\/+$/, '');
-            var pkgfile = x + '/package.json';
+            var base = x.replace(/\/+$/, '');
+            var pkgfile = base + '/package.json';
             if (require.modules[pkgfile]) {
                 var pkg = require.modules[pkgfile]();
                 var b = pkg.browserify;
@@ -70,7 +70,7 @@ require.resolve = (function () {
                 }
             }
             
-            return loadAsFileSync(x + '/index');
+            return loadAsFileSync(base + '/index');
         }
         
         function loadNodeModulesSync (x, start) {
