374d4d09753fd768006f359ab5b61950d998aa4d
James Halliday <mail@substack.net>
2012-01-25 15:44:36 -0800

    failing test if Object.create isn't available

--- test/util.js
+++ test/util.js
@@ -30,3 +30,19 @@ test('util.inherits', function (t) {
     t.ok(beep instanceof EE);
     t.end();
 });
+
+test('util.inherits without Object.create', function (t) {
+    t.plan(2);
+    var src = browserify().require('util').require('events').bundle();
+    var c = { Object : {} };
+    vm.runInNewContext(src, c);
+    var EE = c.require('events').EventEmitter;
+    
+    function Beep () {}
+    c.require('util').inherits(Beep, EE);
+    var beep = new Beep;
+    
+    t.ok(beep instanceof Beep);
+    t.ok(beep instanceof EE);
+    t.end();
+});
