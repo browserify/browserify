ae6a8fceacb007fedbf84eb147e6de055ffb616d
James Halliday <mail@substack.net>
2012-01-25 16:09:08 -0800

    testling tests for util.inherits

--- testling/test.sh
+++ testling/test.sh
@@ -7,5 +7,21 @@ echo -n 'password: '
 read pass
 stty echo
 
-cat <(echo 'process={};') ../wrappers/process.js tick.js \
-    | curl -sSNT- -u "$user:$pass" testling.com
+function tick () {
+    cat <(echo 'process={};') ../wrappers/process.js tick.js \
+        | curl -sSNT- -u "$user:$pass" testling.com
+}
+
+function util () {
+    tar -cf- util.js ../builtins/util.js |
+        curl -sSNT- -u "$user:$pass" 'http://testling.com/?main=util.js'
+}
+
+if test -z "$1"; then
+    tick
+    util
+elif test "$1" == 'tick'; then
+    tick
+elif test "$1" == 'util'; then
+    util
+fi
