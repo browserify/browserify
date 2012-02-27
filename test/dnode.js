c58b34602d81add55e289febf313eb2f9ee8c0a8
James Halliday <mail@substack.net>
2012-02-26 22:48:10 -0800

    take out stupid dnode test

--- test/dnode.js
+++ test/dnode.js
@@ -3,7 +3,7 @@ var browserify = require('../');
 var test = require('tap').test;
 
 test('dnode', function (t) {
-    t.plan(3);
+    t.plan(2);
     
     var src = browserify.bundle({ require : 'dnode' });
     var c = {
@@ -23,21 +23,5 @@ test('dnode', function (t) {
     t.ok(dnode, 'dnode object exists');
     t.ok(dnode.connect, 'dnode.connect exists');
     
-    t.deepEqual(
-        [
-            'path', 'events', 'stream',
-            
-            '/node_modules/dnode/package.json',
-            '/node_modules/dnode/browser/index.js',
-            '/node_modules/dnode/browser/socket.io.js',
-            // ^ move this one later when socket.io supports browserify
-            
-            '/node_modules/dnode/node_modules/dnode-protocol/index.js',
-            '/node_modules/dnode/node_modules/dnode-protocol/package.json',
-            '/node_modules/traverse/index.js',
-            '/node_modules/traverse/package.json',
-        ].sort(),
-        Object.keys(c.require.modules).sort()
-    );
     t.end();
 });
