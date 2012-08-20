f57b7b05119343a06188b1b5c76610af11a21860
James Halliday <mail@substack.net>
2012-08-20 07:58:11 -0700

    also add newlines in debug mode

--- wrappers/entry_debug.js
+++ wrappers/entry_debug.js
@@ -1,2 +1,3 @@
-require.define($__filename,Function(['require','module','exports','__dirname','__filename','process'],$body));
+require.define($__filename,Function(['require','module','exports','__dirname','__filename','process'],$body
+));
 require($__filename);
