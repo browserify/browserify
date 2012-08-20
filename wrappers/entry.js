76ffd1b942b867cb3edccdaa45e66bafb69f4ccb
James Halliday <mail@substack.net>
2012-08-20 07:55:29 -0700

    fix trailing comment newline bug

--- wrappers/entry.js
+++ wrappers/entry.js
@@ -1,2 +1,3 @@
-require.define($__filename,function(require,module,exports,__dirname,__filename,process){$body});
+require.define($__filename,function(require,module,exports,__dirname,__filename,process){$body
+});
 require($__filename);
