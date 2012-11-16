73a2d30ab2e92e592fc9e00ce01753735dbd4392
James Halliday <mail@substack.net>
2012-11-15 18:00:40 -0800

    rip out middleware and watches

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var coffee = require('coffee-script');
 var EventEmitter = require('events').EventEmitter;
 
 var wrap = require('./lib/wrap');
-var watch = require('./lib/watch');
 
 function idFromPath (path) {
     return path.replace(/\\/g, '/');
@@ -17,7 +16,7 @@ function needsNodeModulesPrepended (id) {
     return !/^[.\/]/.test(id) && !isAbsolute(id);
 }
 
-var exports = module.exports = function (entryFile, opts) {
+exports = module.exports = function (entryFile, opts) {
     if (!opts) opts = {};
     
     if (Array.isArray(entryFile)) {
@@ -64,27 +63,6 @@ var exports = module.exports = function (entryFile, opts) {
     w.register('.json', function (body, file) {
         return 'module.exports = ' + body + ';\n';
     });
-    
-    var listening = false;
-    w._cache = null;
-    
-    var self = function (req, res, next) {
-        if (!listening && req.connection && req.connection.server) {
-            req.connection.server.on('close', function () {
-                self.end();
-            });
-        }
-        listening = true;
-        
-        if (req.url.split('?')[0] === (opts.mount || '/browserify.js')) {
-            if (!w._cache) self.bundle();
-            res.statusCode = 200;
-            res.setHeader('last-modified', self.modified.toString());
-            res.setHeader('content-type', 'text/javascript');
-            res.end(w._cache);
-        }
-        else next()
-    };
     
     if (opts.watch) watch(w, opts.watch);
     
@@ -134,67 +112,8 @@ var exports = module.exports = function (entryFile, opts) {
             w.require(opts.require, params);
         }
     }
-
-    if (opts.entry) {
-        if (Array.isArray(opts.entry)) {
-            opts.entry.forEach(function (e) {
-                w.addEntry(e);
-            });
-        }
-        else {
-            w.addEntry(opts.entry);
-        }
-    }
     
-    Object.keys(w).forEach(function (key) {
-        Object.defineProperty(self, key, {
-            set : function (value) { w[key] = value },
-            get : function () { return w[key] }
-        });
-    });
-    
-    Object.keys(Object.getPrototypeOf(w)).forEach(function (key) {
-        self[key] = function () {
-            var s = w[key].apply(self, arguments)
-            if (s === self) { w._cache = null }
-            return s;
-        };
-    });
-    
-    Object.keys(EventEmitter.prototype).forEach(function (key) {
-        if (typeof w[key] === 'function' && w[key].bind) {
-            self[key] = w[key].bind(w);
-        }
-        else {
-            self[key] = w[key];
-        }
-    });
-    
-    var firstBundle = true;
-    self.modified = new Date;
-    
-    self.bundle = function () {
-        if (w._cache) return w._cache;
-        
-        var src = w.bundle.apply(w, arguments);
-        self.ok = Object.keys(w.errors).length === 0;
-        
-        if (!firstBundle) {
-            self.modified = new Date;
-        }
-        firstBundle = false;
-        
-        w._cache = src;
-        return src;
-    };
-    
-    self.end = function () {
-        Object.keys(w.watches || {}).forEach(function (file) {
-            w.watches[file].close();
-        });
-    };
-    
-    return self;
+    return w;
 };
 
 exports.bundle = function (opts) {
