70e311a8424d46abe73596b0ccd2787e94e28456
James Halliday <mail@substack.net>
2012-11-15 18:02:17 -0800

    update alias test to not use middleware

--- test/alias.js
+++ test/alias.js
@@ -1,4 +1,3 @@
-var connect = require('connect');
 var http = require('http');
 var vm = require('vm');
 var browserify = require('../');
@@ -6,53 +5,30 @@ var browserify = require('../');
 var test = require('tap').test;
 
 test('alias', function (t) {
-    t.plan(5);
-    var port = 10000 + Math.floor(Math.random() * (Math.pow(2,16) - 10000));
-    var server = connect.createServer();
+    t.plan(4);
     
-    server.use(browserify({
-        mount : '/bundle.js',
-        require : { moo : 'seq' }
-    }));
+    var src = browserify({ require : { moo : 'seq' } }).bundle();
     
-    server.listen(port, makeRequest);
+    var context = {
+        setTimeout : setTimeout,
+        console : console
+    };
     
-    function makeRequest () {
-        
-        var req = { host : 'localhost', port : port, path : '/bundle.js' };
-        http.get(req, function (res) {
-            t.equal(res.statusCode, 200);
-            server.close();
-            
-            var context = {
-                setTimeout : setTimeout,
-                console : console
-            };
-            
-            var src = '';
-            res.on('data', function (buf) {
-                src += buf.toString();
-            });
-            
-            res.on('end', function () {
-                vm.runInNewContext(src, context);
-                t.ok(context.require('moo'));
-                t.ok(context.require('seq'));
-                t.equal(
-                    String(context.require('seq')),
-                    String(context.require('moo'))
-                );
-                
-                context.require('moo')([1,2,3])
-                    .parMap(function (x) {
-                        this(null, x * 100)
-                    })
-                    .seq(function () {
-                        t.deepEqual(this.stack, [100,200,300]);
-                        t.end();
-                    })
-                ;
-            });
-        });
-    }
+    vm.runInNewContext(src, context);
+    t.ok(context.require('moo'));
+    t.ok(context.require('seq'));
+    t.equal(
+        String(context.require('seq')),
+        String(context.require('moo'))
+    );
+    
+    context.require('moo')([1,2,3])
+        .parMap(function (x) {
+            this(null, x * 100)
+        })
+        .seq(function () {
+            t.deepEqual(this.stack, [100,200,300]);
+            t.end();
+        })
+    ;
 });
