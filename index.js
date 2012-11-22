ebf427df6db4da0268b3fcc353d76e9383c8edb5
James Halliday <mail@substack.net>
2012-11-22 11:44:42 +0900

    using JSONStream with a stubbed-out bundle() implementation

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 var path = require('path');
 var EventEmitter = require('events').EventEmitter;
-var through = require('through');
+var Stream = require('stream');
 
+var inherits = require('inherits');
 var required = require('required');
+var JSONStream = require('JSONStream');
 
-module.exports = function (files, cb) {
-    files = [].concat(files).filter(Boolean);
-    var outStream = through();
+var bundle = require('./lib/bundle');
+
+exports = module.exports = Browserify;
+exports.bundle = bundle;
+inherits(Browserify, Stream);
+
+function Browserify (files) {
+    var self = this;
+    if (!(self instanceof Browserify)) return new Browserify(files);
     
-    required(files[0], function (err, deps) {
-        console.dir(flatten(deps));
+    self.readable = true;
+    self.cache = {};
+    self.deps = {};
+    self.stringify = JSONStream.stringify();
+    
+    [ 'data', 'end', 'close', 'error' ].forEach(function (name) {
+        self.stringify.on(name, self.emit.bind(self, name));
     });
     
-    return outStream;
+    files = [].concat(files).filter(Boolean);
+    files.forEach(function (file) { self.addEntry(file) });
+}
+
+Browserify.prototype.addEntry = function (file) {
+    var self = this;
+    
+    required(file, { cache : self.cache }, function (err, deps) {
+        if (err) return self.emit('error', err);
+        
+        var ds = flatten(deps);
+        Object.keys(ds).forEach(function (file) {
+            if (self.deps[file]) return;
+            
+            self.deps[file] = ds[file];
+            self.stringify.write(ds[file]);
+        });
+    });
+};
+
+Browserify.prototype.require = function () {
+};
+
+Browserify.prototype.bundle = function () {
+    this.pipe(bundle());
 };
 
 function flatten (deps, out) {
-    return deps.reduce(function (acc, d) {
-        if (!acc[d.id]) acc[d.id] = d;
-        return flatten(d.deps, acc);
+    return deps.reduce(function (acc, d_) {
+        var d = shallowCopy(d_);
+        if (!acc[d.filename]) acc[d.filename] = d;
+        
+        var res = flatten(d.deps, acc);
+        d.deps = d.deps.map(function (x) { return x.filename });
+        
+        return res;
     }, out || {});
+}
+
+function shallowCopy (obj) {
+    return Object.keys(obj).reduce(function (acc, key) {
+        acc[key] = obj[key];
+        return acc;
+    }, {});
 }
