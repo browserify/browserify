browserify/browserify

commit a97a3cc661c1806d7cd0a02890893337237d5278

fixes for -browserify modules in node_modules/

lib/wrap.js
@@ -214,9 +214,22 @@ Wrap.prototype.bundle = function () {
         fn.call(this, this);
     }).bind(this));
     
+    (function () {
+        // hack to set -browserify module targets
+        var nm = path.resolve(__dirname, '../node_modules') + '/';
+        Object.keys(self.files).forEach(function (file) {
+            if (file.slice(0, nm.length) !== nm) return;
+            var s = file.slice(nm.length);
+            if (!/^[^.\/]+-browserify\//.test(s)) return;
+            self.files[file].target = '/node_modules/' + s;
+        });
+    })();
+    
     var basedir = (function () {
         var required = Object.keys(self.files)
-            .filter(function (x) { return !self.files[x].target })
+            .filter(function (x) {
+                return !self.files[x].target;
+            })
         ;
         var entries = Object.keys(self.entries)
             .filter(function (x) { return !self.entries[x].target })

commit b41da053122cbf6b3b6f99ae04855c50f28c3bed

Fix -browserify modules, broken by a97a3cc661c1806d7cd0a02890893337237d5278.
Closes #245.

lib/wrap.js
@@ -13,6 +13,7 @@ var nub = require('nub');
 var checkSyntax = require('syntax-error');
 
 var existsSync = fs.existsSync || path.existsSync;
+var sep = path.sep || (process.platform === 'win32' ? '\\' : '/');
 
 module.exports = function (opts) {
     return new Wrap(opts);
@@ -260,12 +261,13 @@ Wrap.prototype.bundle = function () {
     
     (function () {
         // hack to set -browserify module targets
-        var nm = path.resolve(__dirname, '../node_modules') + '/';
+        var nm = path.resolve(__dirname, '../node_modules');
         Object.keys(self.files).forEach(function (file) {
-            if (file.slice(0, nm.length) !== nm) return;
-            var s = file.slice(nm.length);
-            if (!/^[^.\/]+-browserify\//.test(s)) return;
-            self.files[file].target = '/node_modules/' + s;
+            if (file.substring(0, nm.length) !== nm) return;
+            var relativeToNm = path.relative(nm, file);
+            var topDir = relativeToNm.split(sep)[0];
+            if (!/-browserify$/.test(topDir)) return;
+            self.files[file].target = path.join('/', 'node_modules', relativeToNm);
         });
     })();
     