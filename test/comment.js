d8a16cd52339b9eb47809956f78bb02fe89607ef
James Halliday <mail@substack.net>
2013-02-20 08:45:39 +1000

    comment test updated, works with browser-pack 0.1.2

--- test/comment.js
+++ test/comment.js
@@ -5,13 +5,12 @@ var test = require('tap').test;
 test('trailing comment', function (t) {
     t.plan(1);
     var b = browserify(__dirname + '/comment/main.js');
-    b.on('syntaxError', t.fail.bind(t));
-    var src = b.bundle();
-    
-    var c = {
-        ex : function (obj) {
-            t.same(obj, 1234);
-        }
-    };
-    vm.runInNewContext(src, c);
+    b.bundle(function (err, src) {
+        var c = {
+            ex : function (obj) {
+                t.same(obj, 1234);
+            }
+        };
+        vm.runInNewContext(src, c);
+    });
 });
