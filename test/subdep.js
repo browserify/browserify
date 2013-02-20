e2100cad4c5b362b5673f94dac2108800690506a
James Halliday <mail@substack.net>
2013-02-20 18:13:08 +1100

    subdeps test updated, passing

--- test/subdep.js
+++ test/subdep.js
@@ -5,22 +5,12 @@ var test = require('tap').test;
 test('subdep', function (t) {
     t.plan(1);
     
-    var src = browserify.bundle({ require : __dirname + '/subdep/index.js' });
-    var c = {};
-    vm.runInNewContext(src, c);
-    t.deepEqual(
-        Object.keys(c.require.modules).sort(),
-        [
-            '/package.json',
-            '/index.js',
-            '/node_modules/qq/package.json',
-            '/node_modules/qq/b.js',
-            '/node_modules/qq/node_modules/a/package.json',
-            '/node_modules/qq/node_modules/a/index.js',
-            '/node_modules/qq/node_modules/z/index.js',
-            'path',
-            '__browserify_process'
-        ].sort()
-    );
-    t.end();
+    var b = browserify();
+    b.expose('subdep', __dirname + '/subdep/index.js');
+    
+    b.bundle(function (err, src) {
+        var c = {};
+        vm.runInNewContext(src, c);
+        t.equal(c.require('subdep'), 'zzz');
+    });
 });
