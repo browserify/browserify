17d5a0cdc965159c6fd92d353f8d873fd74c4cc1
James Halliday <mail@substack.net>
2013-02-19 23:23:49 -0800

    failing test for __filename and __dirname

--- test/global.js
+++ test/global.js
@@ -16,3 +16,16 @@ test('global', function (t) {
         vm.runInNewContext(src, c);
     });
 });
+
+test('__filename and __dirname', function (t) {
+    t.plan(2);
+    
+    var b = browserify();
+    b.add(__dirname + '/global/filename.js');
+    b.bundle(function (err, src) {
+        var c = {};
+        vm.runInNewContext(src, c);
+        t.equal(c.filename, '/filename.js');
+        t.equal(c.dirname, '/');
+    });
+});
