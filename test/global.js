8d05c0bbfa20e2866202a8ff998fa9f3192776bb
James Halliday <mail@substack.net>
2013-02-20 16:07:32 +1000

    updated global test, fails

--- test/global.js
+++ test/global.js
@@ -5,15 +5,14 @@ var test = require('tap').test;
 test('global', function (t) {
     t.plan(2);
     
-    var src = browserify()
-        .addEntry(__dirname + '/global/main.js')
-        .bundle()
-    ;
-    
-    var c = {
-        t : t,
-        a : 555,
-    };
-    c.window = c;
-    vm.runInNewContext(src, c);
+    var b = browserify();
+    b.add(__dirname + '/global/main.js');
+    b.bundle(function (err, src) {
+        var c = {
+            t : t,
+            a : 555,
+        };
+        c.window = c;
+        vm.runInNewContext(src, c);
+    });
 });
