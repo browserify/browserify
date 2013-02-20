b3736d1821d37745a597db46c6d0e05966001ddf
James Halliday <mail@substack.net>
2013-02-20 18:15:33 +1100

    updated the util test, passing

--- test/util.js
+++ test/util.js
@@ -5,44 +5,56 @@ var vm = require('vm');
 
 test('util.inspect', function (t) {
     t.plan(1);
-    var src = browserify().require('util').bundle();
-    var c = {};
-    vm.runInNewContext(src, c);
-    t.equal(
-        c.require('util').inspect([1,2,3]),
-        util.inspect([1,2,3])
-    );
-    t.end();
+    
+    var b = browserify();
+    b.require('util');
+    b.bundle(function (err ,src) {
+        var c = {};
+        vm.runInNewContext(src, c);
+        t.equal(
+            c.require('util').inspect([1,2,3]),
+            util.inspect([1,2,3])
+        );
+    });
 });
 
 test('util.inherits', function (t) {
     t.plan(2);
-    var src = browserify().require('util').require('events').bundle();
-    var c = {};
-    vm.runInNewContext(src, c);
-    var EE = c.require('events').EventEmitter;
     
-    function Beep () {}
-    c.require('util').inherits(Beep, EE);
-    var beep = new Beep;
+    var b = browserify();
+    b.require('util');
+    b.require('events');
     
-    t.ok(beep instanceof Beep);
-    t.ok(beep instanceof EE);
-    t.end();
+    b.bundle(function (err, src) {
+        var c = {};
+        vm.runInNewContext(src, c);
+        var EE = c.require('events').EventEmitter;
+        
+        function Beep () {}
+        c.require('util').inherits(Beep, EE);
+        var beep = new Beep;
+        
+        t.ok(beep instanceof Beep);
+        t.ok(beep instanceof EE);
+    });
 });
 
 test('util.inherits without Object.create', function (t) {
     t.plan(2);
-    var src = browserify().require('util').require('events').bundle();
-    var c = { Object : {} };
-    vm.runInNewContext(src, c);
-    var EE = c.require('events').EventEmitter;
+    var b = browserify();
+    b.require('util');
+    b.require('events');
     
-    function Beep () {}
-    c.require('util').inherits(Beep, EE);
-    var beep = new Beep;
-    
-    t.ok(beep instanceof Beep);
-    t.ok(beep instanceof EE);
-    t.end();
+    b.bundle(function (err, src) {
+        var c = { Object : {} };
+        vm.runInNewContext(src, c);
+        var EE = c.require('events').EventEmitter;
+        
+        function Beep () {}
+        c.require('util').inherits(Beep, EE);
+        var beep = new Beep;
+        
+        t.ok(beep instanceof Beep);
+        t.ok(beep instanceof EE);
+    });
 });
