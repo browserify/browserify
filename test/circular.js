5431a52bd05e599ffda1fcba642756bcae6003f9
James Halliday <mail@substack.net>
2013-03-21 00:33:20 -0700

    other passing test for circulars, passes

--- test/circular.js
+++ test/circular.js
@@ -21,3 +21,14 @@ test('circular expose', function (t) {
         vm.runInNewContext(src, { t: t });
     });
 });
+
+test('circular require', function (t) {
+    t.plan(1);
+    
+    var b = browserify(__dirname + '/circular/main.js');
+    b.require(__dirname + '/circular/a.js');
+    b.require(__dirname + '/circular/b.js');
+    b.bundle(function (err, src) {
+        vm.runInNewContext(src, { t: t });
+    });
+});
