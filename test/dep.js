4f4dac74b1c4f5c889c149efed6008a23b1b8bcb
James Halliday <mail@substack.net>
2013-05-29 16:13:08 -0700

    passing test for dep events

---
+++ test/dep.js
@@ -0,0 +1,27 @@
+var browserify = require('../');
+var vm = require('vm');
+var test = require('tap').test;
+
+test('dependency events', function (t) {
+    t.plan(4);
+    var b = browserify(__dirname + '/entry/main.js');
+    var deps = [];
+    b.on('dep', function (row) {
+        deps.push({ id: row.id, deps: row.deps });
+        t.equal(typeof row.source, 'string');
+    });
+    
+    b.bundle(function (err, src) {
+        t.deepEqual(deps, [
+            {
+                id: __dirname + '/entry/main.js',
+                deps: {
+                    './one': __dirname + '/entry/one.js',
+                    './two': __dirname + '/entry/two.js',
+                }
+            },
+            { id: __dirname + '/entry/one.js', deps: {} },
+            { id: __dirname + '/entry/two.js', deps: {} }
+        ]);
+    });
+});
