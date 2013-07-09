e9bd769d067b33380d058bfd8a515228fc486bcf
James Halliday <mail@substack.net>
2013-07-08 17:17:48 -0700

    update the crypto test, still fails

--- test/crypto.js
+++ test/crypto.js
@@ -3,15 +3,13 @@ var spawn = require('child_process').spawn;
 var path = require('path');
 var fs = require('fs');
 var vm = require('vm');
+var concat = require('concat-stream');
 
 var mkdirp = require('mkdirp');
-var tmpdir = '/tmp/' + Math.random().toString(16).slice(2);
+var tmpdir = '/tmp/browserify-test/' + Math.random().toString(16).slice(2);
 mkdirp.sync(tmpdir);
 
-fs.writeFileSync(tmpdir + '/main.js', [
-    'var c = require("crypto")',
-    'beep(c)'
-].join('\n'));
+fs.writeFileSync(tmpdir + '/main.js', 'beep(require("crypto"))\n');
 
 test('*-browserify libs from node_modules/', function (t) {
     t.plan(2);
@@ -19,19 +17,19 @@ test('*-browserify libs from node_modules/', function (t) {
     var bin = __dirname + '/../bin/cmd.js';
     var ps = spawn(bin, [ 'main.js' ], { cwd : tmpdir });
     
-    var src = '';
-    ps.stdout.on('data', function (buf) { src += buf });
     ps.stderr.pipe(process.stderr, { end : false });
     
     ps.on('exit', function (code) {
         t.equal(code, 0);
-        
+    });
+    
+    ps.stdout.pipe(concat(function (src) {
         var c = {
             beep : function (c) {
                 t.equal(typeof c.createHash, 'function');
-            },
+            }
         };
-        vm.runInNewContext(src, c);
+        vm.runInNewContext(src.toString('utf8'), c);
         t.end();
-    });
+    }));
 });
