111444f385dd0cd8611a3a0933e92b05bd9c0daf
Randall Leeds <randall@bleeds.info>
2013-11-13 14:39:24 -0800

    Add failing test for trailing ; after source map
    Ref #496

--- test/bundle_sourcemap.js
+++ test/bundle_sourcemap.js
@@ -5,13 +5,14 @@ test('bundle in debug mode', function (t) {
     var b = browserify();
     b.require('seq');
     b.bundle({ debug: true }, function (err, src) {
-        t.plan(3);
-        
+        t.plan(4);
+
         var secondtolastLine = src.split('\n').slice(-2);
 
         t.ok(typeof src === 'string');
         t.ok(src.length > 0);
         t.ok(/^\/\/@ sourceMappingURL=/.test(secondtolastLine), 'includes sourcemap');
+        t.ok(src.split('\n').slice(-1)[0].length == 0, 'source map is the last non-empty line');
     });
 });
 
