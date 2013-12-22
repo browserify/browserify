1abc346fc82cf6d58266b5fcb5dc7f871542ee43
James Halliday <mail@substack.net>
2013-12-21 21:11:51 -0800

    failing implicit stdin test

--- test/stdin.js
+++ test/stdin.js
@@ -33,3 +33,32 @@ test('stdin', function (t) {
     ps.stdin.end();
     
 });
+
+test('stdin !isTTY', function (t) {
+    t.plan(2);
+    
+    var cwd = process.cwd();
+    process.chdir(__dirname);
+    
+    var ps = spawn(process.execPath, [
+        path.resolve(__dirname, '../bin/cmd.js')
+    ]);
+    
+    ps.stdout.pipe(concat(function (body) {
+        var c = { console: {
+            log: function (msg) {
+                t.equal(msg, 'hello');
+            }
+        } };
+        vm.runInNewContext(body, c);
+    }));
+    ps.stderr.pipe(process.stderr);
+    
+    ps.on('exit', function (code) {
+        t.equal(code, 0);
+    });
+    
+    ps.stdin.write("console.log('hello')");
+    ps.stdin.end();
+    
+});
