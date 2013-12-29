39720187e426bb50fd834570a49b949ecba58ced
James Halliday <mail@substack.net>
2013-12-29 08:39:37 -0800

    test for the new A.B.C standalone behavior

--- test/standalone.js
+++ test/standalone.js
@@ -38,6 +38,40 @@ test('standalone', function (t) {
     });
 });
 
+test('A.B.C standalone', function (t) {
+    t.plan(3);
+    
+    var b = browserify(__dirname + '/standalone/main.js');
+    b.bundle({standalone: 'A.B.C'}, function (err, src) {
+        t.test('window global', function (t) {
+            t.plan(2);
+            var c = { window: {} };
+            vm.runInNewContext(src, c);
+            c.window.A.B.C(done(t));
+        });
+        t.test('CommonJS', function (t) {
+            t.plan(2);
+            var exp = {};
+            var c = {
+                module: { exports: exp },
+                exports: exp
+            };
+            vm.runInNewContext(src, c);
+            c.module.exports(done(t));
+        });
+        t.test('RequireJS', function (t) {
+            t.plan(2);
+            var c = {
+                define: function (fn) {
+                    fn()(done(t));
+                }
+            };
+            c.define.amd = true;
+            vm.runInNewContext(src, c);
+        });
+    });
+});
+
 function done(t) {
     return function (one, two) {
         t.equal(one, 1);
