6f042cc3051b27d50903dc91d16498529e6d8007
James Halliday <mail@substack.net>
2014-01-20 19:11:06 -0800

    fix for the retarget test

--- bin/args.js
+++ bin/args.js
@@ -37,14 +37,14 @@ module.exports = function (args) {
         if (entry === '-') return process.stdin;
         return path.resolve(process.cwd(), entry);
     });
+    var requires = [].concat(argv.r, argv.require).filter(Boolean);
     
-    if (entries.length === 0 && !process.stdin.isTTY) {
+    if (entries.length + requires.length === 0 && !process.stdin.isTTY) {
         entries.push(process.stdin);
     }
     
-    if (argv.s && entries.length === 0
-    && [].concat(argv.r, argv.require).filter(Boolean).length === 1) {
-        entries.push([].concat(argv.r, argv.require).filter(Boolean)[0]);
+    if (argv.s && entries.length === 0 && requires.length === 1) {
+        entries.push(requires[0]);
         argv.r = argv.require = [];
     }
     
