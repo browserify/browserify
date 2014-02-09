55e0b6aec7bf0a9c51af4818258d1194e5950fb6
James Halliday <mail@substack.net>
2014-02-08 23:41:23 -0800

    failing transform argument test

---
+++ test/tr_args/tr.js
@@ -0,0 +1,12 @@
+var through = require('through');
+
+module.exports = function (file, opts) {
+    var data = '';
+    return through(write, end);
+    
+    function write (buf) { data += buf }
+    function end () {
+        this.queue(data.replace(/X/g, opts.x);
+        this.queue(null);
+    }
+};
