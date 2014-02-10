9c768369e04099984036f826c8ea541de70f6aef
James Halliday <mail@substack.net>
2014-02-10 10:50:45 -0800

    subtests are dumb

--- test/multi_entry.js
+++ test/multi_entry.js
@@ -3,56 +3,52 @@ var vm = require('vm');
 var test = require('tap').test;
 var fs = require('fs');
 
-test('entries as string paths', function (t) {
-    var testFiles = [
-        __dirname + '/multi_entry/a.js',
-        __dirname + '/multi_entry/b.js',
-        __dirname + '/multi_entry/c.js'
-    ];
+var testFiles = [
+    __dirname + '/multi_entry/a.js',
+    __dirname + '/multi_entry/b.js',
+    __dirname + '/multi_entry/c.js'
+];
 
-    t.test('multi entry', function (t) {
-        t.plan(3);
+test('multi entry', function (t) {
+    t.plan(3);
+    
+    var b = browserify([
+        testFiles[0],
+        testFiles[1]
+    ]);
+    b.add(testFiles[2]);
 
-        var b = browserify([
-            testFiles[0],
-            testFiles[1]
-        ]);
-        b.add(testFiles[2]);
-
-        b.bundle(function (err, src) {
-            var c = {
-                times : 0,
-                t : t
-            };
-            vm.runInNewContext(src, c);
-        });
+    b.bundle(function (err, src) {
+        var c = {
+            times : 0,
+            t : t
+        };
+        vm.runInNewContext(src, c);
     });
+});
 
-
-    // This test should blink if stream order is not respected
-    t.test('entries as streams', function (t) {
-        t.plan(3);
-
-        // transform paths to streams
-        for (var i = 0; i < testFiles.length; i++) {
-            testFiles[i] = fs.createReadStream(testFiles[i]);
-        }
-
-        // commondir blows up with streams and without basedir
-        var opts = {basedir: __dirname + '/multi_entry'}
-
-        var b = browserify([
-            testFiles[0],
-            testFiles[1]
-        ], opts);
-        b.add(testFiles[2]);
-
-        b.bundle(function (err, src) {
-            var c = {
-                times : 0,
-                t : t
-            };
-            vm.runInNewContext(src, c);
-        });
+test('entries as streams', function (t) {
+    t.plan(3);
+    
+    // transform paths to streams
+    for (var i = 0; i < testFiles.length; i++) {
+        testFiles[i] = fs.createReadStream(testFiles[i]);
+    }
+    
+    // commondir blows up with streams and without basedir
+    var opts = { basedir: __dirname + '/multi_entry' };
+    
+    var b = browserify([
+        testFiles[0],
+        testFiles[1]
+    ], opts);
+    b.add(testFiles[2]);
+    
+    b.bundle(function (err, src) {
+        var c = {
+            times : 0,
+            t : t
+        };
+        vm.runInNewContext(src, c);
     });
 });
