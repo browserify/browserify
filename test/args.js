d7fddbdb243c9ea01b2ed0a50f25e392b6084851
David Björklund <david.bjorklund@gmail.com>
2014-02-24 14:25:26 +0100

    parse vars correctly when using --insert-global-vars

--- test/args.js
+++ test/args.js
@@ -13,3 +13,17 @@ test('bundle from an arguments array', function (t) {
         t.equal(c.window.XYZ, 2);
     });
 });
+
+test('bundle from an arguments with --insert-global-vars', function (t) {
+    t.plan(2)
+
+    var b = fromArgs([ __dirname + '/global/filename.js', '--insert-global-vars=__filename,__dirname' ]);
+    b.expose('x', __dirname + '/global/filename.js');
+    b.bundle({ basedir: __dirname }, function (err, src) {
+        var c = {};
+        vm.runInNewContext(src, c);
+        var x = c.require('x');
+        t.equal(x.filename, '/global/filename.js');
+        t.equal(x.dirname, '/global');
+    })
+});
