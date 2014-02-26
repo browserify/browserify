1d24e6bd83e6aee404ff71032385fa52c8a199ce
James Halliday <mail@substack.net>
2014-02-25 16:48:34 -0800

    fix the tests to set a basedir explicitly

--- test/id_hash.js
+++ test/id_hash.js
@@ -1,9 +1,11 @@
 var browserify = require('../');
 var test = require('tap').test;
+var path = require('path');
+var opts = { basedir: path.dirname(__dirname) };
 
 test('idHash required deps', function (t) {
     t.plan(1);
-    var b = browserify();
+    var b = browserify(opts);
     b.require(__dirname + '/id_hash/require.js', { expose: 'idhashdep' });
     b.bundle(function (err, src) {
         t.ok(/\'GfayEO\'/.test(src), 'Output contains valid hash');
@@ -12,7 +14,7 @@ test('idHash required deps', function (t) {
 
 test('idHash external deps', function (t) {
     t.plan(1);
-    var b = browserify(__dirname + '/id_hash/main.js');
+    var b = browserify(__dirname + '/id_hash/main.js', opts);
     b.external(__dirname + '/id_hash/require.js');
     b.bundle(function (err, src) {
         t.ok(/GfayEO/.test(src), 'Output contains valid hash');
@@ -21,7 +23,10 @@ test('idHash external deps', function (t) {
 
 test('idHash exposeAll', function (t) {
     t.plan(1);
-    var b = browserify(__dirname + '/id_hash/require.js', { exposeAll: true });
+    var b = browserify(__dirname + '/id_hash/require.js', {
+        exposeAll: true,
+        basedir: opts.basedir
+    });
     b.bundle(function (err, src) {
         t.ok(/GfayEO/.test(src), 'Output contains valid hash');
     });
