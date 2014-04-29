0e6979dcbeb47b6a6174096701ce39af72348c30
Oscar Morante <oscar.morante@mirada.tv>
2014-04-29 13:12:15 +0300

    allow arrays in require and external

--- index.js
+++ index.js
@@ -146,6 +146,9 @@ Browserify.prototype.require = function (id, opts) {
         self.files.push(id);
         if (opts.entry) self._entries.push(id.path);
         return self;
+    } else if (Array.isArray(id)) {
+        id.forEach(function(id) { self.require(id, opts); });
+        return self;
     }
     
     if (opts === undefined) opts = { expose: id };
@@ -237,6 +240,9 @@ Browserify.prototype.external = function (id, opts) {
         }
         if (id._pending === 0) return captureDeps();
         return id.once('_ready', captureDeps);
+    } else if (Array.isArray(id)) {
+        id.forEach(function(id) { self.external(id, opts); });
+        return self;
     }
     
     opts.external = true;
