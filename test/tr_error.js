b207b3046c715bcd58e4f3031409b94784255a48
James Halliday <mail@substack.net>
2014-05-14 02:28:15 +0200

    failing tests for transform error propagation

---
+++ test/tr_error.js
@@ -0,0 +1,34 @@
+var browserify = require('../');
+var vm = require('vm');
+var test = require('tap').test;
+var through = require('through');
+
+test('transform errors errback', function (t) {
+    t.plan(1);
+    
+    var b = browserify(__dirname + '/tr/main.js');
+    b.transform(function (file) {
+        return through(function (buf) {
+            this.emit('error', new Error('blah'));
+        })
+    });
+    b.bundle(function (err, src) {
+        t.equal(err.message, 'blah');
+        t.equal(src, undefined);
+    });
+});
+
+test('transform errors propagate', function (t) {
+    t.plan(1);
+    
+    var b = browserify(__dirname + '/tr/main.js');
+    b.transform(function (file) {
+        return through(function (buf) {
+            this.emit('error', new Error('blah'));
+        })
+    });
+    b.on('error', function (err) {
+        t.equal(err.message, 'blah');
+    });
+    b.bundle();
+});
