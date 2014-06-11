a1b5fa457c05b9a152ca56ab613bafc0ac16852c
James Halliday <mail@substack.net>
2014-06-10 22:40:43 -0700

    failing pipeline test

---
+++ test/pipeline_deps.js
@@ -0,0 +1,21 @@
+var browserify = require('../');
+var vm = require('vm');
+var test = require('tap').test;
+var through = require('through2');
+
+test('deps pipeline', function (t) {
+    t.plan(1);
+    
+    var b = browserify(__dirname + '/entry/main.js');
+    b.pipeline.get('deps').push(through.obj(function (row, enc, next) {
+        row.source = row.source.replace(/111/g, '11111');
+        next();
+    }));
+    
+    b.bundle(function (err, src) {
+        Function([ 'console' ], src)({ log: log });
+        function log (msg) {
+            t.equal(msg, 'main: 56055');
+        }
+    });
+});
