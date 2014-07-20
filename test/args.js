5d70505b8baf67421a9b870f67cf7cf26260a5e8
James Halliday <mail@substack.net>
2014-07-20 16:25:22 -0700

    improve args test

--- test/args.js
+++ test/args.js
@@ -4,10 +4,11 @@ var path = require('path');
 var vm = require('vm');
 
 test('bundle from an arguments array', function (t) {
-    t.plan(1);
+    t.plan(2);
     
     var b = fromArgs([ __dirname + '/entry/two.js', '-s', 'XYZ' ]);
     b.bundle(function (err, src) {
+        t.ifError(err);
         var c = { window: {} };
         vm.runInNewContext(src, c);
         t.equal(c.window.XYZ, 2);
@@ -15,20 +16,22 @@ test('bundle from an arguments array', function (t) {
 });
 
 test('external flag for node modules', function(t) {
-    t.plan(1);
+    t.plan(2);
     
     var b = fromArgs([ __dirname + '/external_args/main.js', '-x', 'backbone' ]);
     b.bundle(function (err, src) {
+        t.ifError(err);
         vm.runInNewContext(src, {t: t});
     });
 });
 
 test('bundle from an arguments with --insert-global-vars', function (t) {
-    t.plan(2)
+    t.plan(3)
 
     var b = fromArgs([ __dirname + '/global/filename.js', '--insert-global-vars=__filename,__dirname' ]);
     b.require(__dirname + '/global/filename.js', { expose: 'x' });
     b.bundle({ basedir: __dirname }, function (err, src) {
+        t.ifError(err);
         var c = {};
         vm.runInNewContext(src, c);
         var x = c.require('x');
