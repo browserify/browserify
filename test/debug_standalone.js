baa36f3ab98d123b9da0ff91d5983f36ec3437ba
James Halliday <mail@substack.net>
2014-07-24 00:57:21 -0700

    first part of debug standalone test

---
+++ test/debug_standalone.js
@@ -0,0 +1,23 @@
+var test = require('tap').test;
+var browserify = require('../');
+var through = require('through2');
+var vm = require('vm');
+
+test('ordinary debug', function (t) {
+    t.plan(1);
+    
+    var stream = through();
+    stream.push('console.log(1+2)');
+    stream.push(null);
+    
+    var b = browserify({ debug: true });
+    b.add(stream);
+    b.bundle(function (err, buf) {
+        var src = buf.toString('utf8');
+        var last = src.split('\n').slice(-1)[0];
+        t.ok(
+            /\/\/# sourceMappingURL=data:application\/json;base64,[\w+\/=]+$/
+            .test(last)
+        );
+    });
+});
