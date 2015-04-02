645669e065560fb098304c93d169003c87d27bd4
Terin Stock <terinjokes@gmail.com>
2015-04-02 15:58:05 -0700

    fixup "$NODE_PATH" test to not use "NODE_PATHS".

--- test/paths.js
+++ test/paths.js
@@ -1,14 +1,15 @@
 var browserify = require('../');
 var vm = require('vm');
+var path = require('path');
 var test = require('tap').test;
 
-test('$NODE_PATHS', function (t) {
+test('$NODE_PATH', function (t) {
     t.plan(3);
     var paths = [ __dirname + '/paths/x', __dirname + '/paths/y' ];
-    var sep = /^win/i.test(process.platform) ? ';' : ':';
+    var delimiter = path.delimiter || (process.platform === 'win32' ? ';' : ':');
     
-    process.env.NODE_PATH = (process.env.NODE_PATHS || '')
-        .split(sep).concat(paths).join(sep)
+    process.env.NODE_PATH = (process.env.NODE_PATH || '')
+        .split(delimiter).concat(paths).join(delimiter)
     ;
     
     var b = browserify(__dirname + '/paths/main.js');
