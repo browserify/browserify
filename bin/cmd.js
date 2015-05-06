a2288659c7a0ac9e6a6760edf61e5b147d27f232
Chad Hietala <chadhietala@gmail.com>
2015-05-06 13:27:22 -0700

    Replace JSONStream with jsonstream
    NPM now has opinions about names of modules when publishing. We have an internal NPM registry and cannot sync browserify to is due to this dependency. No fear, @dominictarr has published a lowercase version.

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var JSONStream = require('JSONStream');
+var JSONStream = require('jsonstream');
 var through = require('through2');
 
 var b = require('./args')(process.argv.slice(2));
