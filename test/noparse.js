1d750308405e9bb7c249d86b8000af0921a867e1
Andres Suarez <zertosh@gmail.com>
2015-05-09 00:14:34 -0400

    Add test for "noParse"

---
+++ test/noparse.js
@@ -0,0 +1,29 @@
+var browserify = require('../');
+var test = require('tap').test;
+var path = require('path');
+
+test('noParse array', function (t) {
+    t.plan(2);
+    
+    var actual = [];
+    var expected = [
+        'noparse/a.js',
+        'noparse/b.js',
+        'noparse/dir1/1.js',
+        'noparse/node_modules/robot/main.js'
+    ].map(function (x) {return path.resolve(x);}).sort();
+    
+    var b = browserify({
+        entries: [ __dirname + '/noparse/a.js' ],
+        noParse: [
+            __dirname + '/noparse/dir1/1.js',
+            __dirname + '/noparse/node_modules/robot/main.js'
+        ]
+    });
+    b.on('dep', function(dep) { actual.push(dep.file); });
+    b.bundle(function (err, src) {
+        actual.sort();
+        t.ifError(err);
+        t.deepEqual(actual, expected);
+    });
+});
