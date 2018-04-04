2fbc2bdd39a0082516da3eb4cf05aa087dab7a76
Renée Kooi <renee@kooi.me>
2018-04-04 14:41:24 +0200

    Add test for #1654

---
+++ test/browser_field_builtin.js
@@ -0,0 +1,23 @@
+var test = require('tap').test;
+var fs = require('fs');
+var path = require('path');
+var vm = require('vm');
+var browserify = require('../');
+var temp = require('temp');
+temp.track();
+var tmpdir = temp.mkdirSync({prefix: 'browserify-test'});
+
+test('no browser field builtin', function (t) {
+    t.plan(2);
+    var src = fs.readFileSync(path.join(__dirname, '/browser_field_builtin/main.js'));
+    fs.writeFileSync(path.join(tmpdir, 'main.js'), src);
+    var b = browserify({
+        entries: path.join(tmpdir, 'main.js'),
+        browserField: false
+    });
+    b.bundle(function (err, src) {
+        t.ifError(err);
+        vm.runInNewContext(src, { console: { log: log } });
+        function log (msg) { t.deepEqual(msg, { a: 'b' }) }
+    });
+});
