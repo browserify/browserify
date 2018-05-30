38f7e1b6b8b9c646086352bb424f1f922b2085dd
Renée Kooi <renee@kooi.me>
2018-05-30 14:41:05 +0200

    Upgrade events to v3.0.0.

--- test/util.js
+++ test/util.js
@@ -46,7 +46,7 @@ test('util.inherits without Object.create', function (t) {
     b.require('events');
     
     b.bundle(function (err, src) {
-        var c = { Object : { prototype: Object.prototype } };
+        var c = { Object : { prototype: Object.prototype, defineProperty: Object.defineProperty } };
         vm.runInNewContext(src, c);
         var EE = c.require('events').EventEmitter;
         
