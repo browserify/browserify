c66b95f6afd8771fbf792ccc5c4fd4508d61af3d
Chris Rawnsley <chris@puny.agency>
2018-10-26 11:57:18 +0100

    Add failing test case for --list --require [module]

--- test/bin_list.js
+++ test/bin_list.js
@@ -37,3 +37,37 @@ test('bin --list', function (t) {
         t.ok(allDone);
     });
 });
+
+test('bin --require --list', function (t) {
+    t.plan(4);
+
+    var cwd = process.cwd();
+    process.chdir(path.resolve(__dirname, 'list'));
+
+    var ps = spawn(process.execPath, [
+        path.resolve(__dirname, '../bin/cmd.js'),
+        '--list', '--require', 'qq'
+    ]);
+    var src = '';
+    var err = '';
+    ps.stdout.on('data', function (buf) { src += buf });
+    ps.stderr.on('data', function (buf) { err += buf });
+
+    var expected = [
+      path.resolve(__dirname, 'list/node_modules/qq/node_modules/z/index.js'),
+      path.resolve(__dirname, 'list/node_modules/qq/node_modules/a/index.js'),
+      path.resolve(__dirname, 'list/node_modules/qq/b.js')
+    ].join('\n') + '\n';
+
+    ps.on('exit', function (code) {
+        t.equal(code, 0);
+        t.equal(err, '');
+        t.equal(src, expected);
+
+        var allDone = false;
+        var c = { done : function () { allDone = true } };
+
+        vm.runInNewContext(src, c);
+        t.ok(allDone);
+    });
+});
